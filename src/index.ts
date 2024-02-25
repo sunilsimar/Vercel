8268ccbebfeec8ba27f9a687ee2795d49bee1072
Sunil <sunisimar2002d@gmail.com>
2024-02-26 03:24:32 +0530

    Deploy endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,23 @@
 import express from 'express';
 import cors from 'cors';
 import simpleGit from 'simple-git';
+import path from 'path';
 import { generate } from './utils';
+import { getAllFiles } from './file';
 
 const app = express();
 app.use(cors())
 app.use(express.json())
 
+
 app.post('/deploy',async(req,res)=>{
     const repoUrl = req.body.repoUrl;
     const id = generate();
-    await simpleGit().clone(repoUrl, `output/${id}`);
+    await simpleGit().clone(repoUrl, path.join(__dirname ,`output/${id}`));
 
-    res.json({"RepoUrl":"done"})
+    const files = getAllFiles(path.join(__dirname, `output/${id}`))
+
+    res.json({id: id})
 })
 
 app.listen(3000);
--- src/utils.ts
+++ src/utils.ts
@@ -6,4 +6,5 @@ export function generate(){
     for(let i=0; i<MAX_LEN; i++){
         ans += subset[Math.floor(Math.random() * subset.length)];
     }
+    return ans;
 }
