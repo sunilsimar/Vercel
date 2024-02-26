c3324821e4a766016668841eada643fe516a6638
Sunil <sunisimar2002d@gmail.com>
2024-02-26 13:51:54 +0530

    done

--- upload-service/src/aws.ts
+++ upload-service/src/aws.ts
@@ -1,19 +1,18 @@
-import { S3 } from 'aws-sdk';
-import fs from 'fs';
+import { S3 } from "aws-sdk";
+import fs from "fs";
 
 const s3 = new S3({
-    accessKeyId: "",
-    secretAccessKey: "",
-    endpoint: "",
+    accessKeyId: "7ea9c3f8c7f0f26f0d21c5ce99d1ad6a",
+    secretAccessKey: "b4df203781dd711223ce931a2d7ca269cdbf81bb530de4548474584951b798be",
+    endpoint: "https://e21220f4758c0870ba9c388712d42ef2.r2.cloudflarestorage.com"
 })
 
-export const uploadFile = async(filename: string, localFilePath: string) => {
-    console.log('called');
+export const uploadFile = async (fileName: string, localFilePath: string) => {
     const fileContent = fs.readFileSync(localFilePath);
     const response = await s3.upload({
         Body: fileContent,
         Bucket: "vercel",
-        Key: filename,
+        Key: fileName,
     }).promise();
     console.log(response);
 }
--- upload-service/src/index.ts
+++ upload-service/src/index.ts
@@ -1,34 +1,41 @@
-import express from 'express';
-import cors from 'cors';
-import simpleGit from 'simple-git';
-import path from 'path';
-import { generate } from './utils';
-import { getAllFiles } from './file';
-import { uploadFile } from './aws';
-import {createClient} from 'redis';
 
+import express from "express";
+import cors from "cors";
+import simpleGit from "simple-git";
+import { generate } from "./utils";
+import { getAllFiles } from "./file";
+import path from "path";
+import { uploadFile } from "./aws";
+import { createClient } from "redis";
 const publisher = createClient();
-publisher.connect()
+publisher.connect();
+
+const subscriber = createClient();
+subscriber.connect();
 
 const app = express();
 app.use(cors())
-app.use(express.json())
+app.use(express.json());
 
-
-app.post('/deploy',async(req,res)=>{
+app.post("/deploy", async (req, res) => {
     const repoUrl = req.body.repoUrl;
-    const id = generate();
-    await simpleGit().clone(repoUrl, path.join(__dirname ,`output/${id}`));
+    const id = generate(); // asd12
+    await simpleGit().clone(repoUrl, path.join(__dirname, `output/${id}`));
 
-    const files = getAllFiles(path.join(__dirname, `output/${id}`))
-    
+    const files = getAllFiles(path.join(__dirname, `output/${id}`));
+
     files.forEach(async file => {
         await uploadFile(file.slice(__dirname.length + 1), file);
     })
 
+    await new Promise((resolve) => setTimeout(resolve, 5000))
     publisher.lPush("build-queue", id);
 
-    res.json({id: id})
-})
+    res.json({
+        id: id
+    })
+
+});
+
 
 app.listen(3000);
